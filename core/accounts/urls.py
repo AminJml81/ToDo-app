d7d1f14253ab67a6628a67841d424ef9df77aa86
Amin <Aminbest1381@gmail.com>
2024-08-19 15:13:59 +0330

    added password change.

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from django.contrib.auth.views import (
-    LoginView, LogoutView)
+    LoginView, LogoutView,
+    )
 
-from accounts.views import SignUpView
+from accounts.views import SignUpView, CustomPasswordChangeView, manage_account_view
+from django.urls import reverse_lazy
 
 app_name = 'account'
 
 urlpatterns = [
+    path('', manage_account_view, name='manage'),
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('logout/', LogoutView.as_view(template_name = "registration/logout.html"), name='logout')
+    path('logout/', LogoutView.as_view(template_name = "registration/logout.html"), name='logout'),
+    path('password/change/', CustomPasswordChangeView.as_view(), name='change-password'),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
-from accounts.models import User
 from django.urls import reverse_lazy
-from accounts.forms import SignUpForm
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib import messages
+
 # Create your views here.
+from accounts.models import User
+from accounts.forms import SignUpForm
+
+
+def manage_account_view(request):
+    return render(request, 'registration/manage_account.html')
 
 
 class SignUpView(CreateView):
@@ -11,3 +18,12 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('todo:task-list')
     template_name = 'registration/signup.html'
     form_class = SignUpForm
+
+class CustomPasswordChangeView(PasswordChangeView):
+     
+     success_url = reverse_lazy("account:login")
+
+     def form_valid(self, form):
+        messages.success(self.request, 'Password change successful' )
+        return super().form_valid(form)
+    
