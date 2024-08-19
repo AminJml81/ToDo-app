4cada26a77b5f1afa8fed114835ceb2abc535f0b
Amin <Aminbest1381@gmail.com>
2024-08-19 17:38:10 +0330

    added password reset functionality

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import (
     LoginView, LogoutView,
+    PasswordResetView, PasswordResetConfirmView, 
+    PasswordResetDoneView, PasswordResetCompleteView
     )
 
 from accounts.views import SignUpView, CustomPasswordChangeView, manage_account_view
@@ -12,6 +14,18 @@ urlpatterns = [
     path('', manage_account_view, name='manage'),
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('logout/', LogoutView.as_view(template_name = "registration/logout.html"), name='logout'),
-    path('password/change/', CustomPasswordChangeView.as_view(), name='change-password'),
+    path('logout/',
+         LogoutView.as_view(template_name = "registration/logout.html"), name='logout'),
+
+    path('password/change/',
+          CustomPasswordChangeView.as_view(), name='change-password'),
+          
+    path('password/reset/',
+          PasswordResetView.as_view(success_url=reverse_lazy("account:reset-password-done")), name='reset-password'),
+    path('password/reset/done/',
+          PasswordResetDoneView.as_view(), name='reset-password-done'),
+    path('password/reset/confirm/<uidb64>/<token>/',
+          PasswordResetConfirmView.as_view(success_url = reverse_lazy("account:password-reset-complete")), name='reset-password-confirm'),
+    path('password/reset/complete/',
+          PasswordResetCompleteView.as_view(), name='password-reset-complete' )
 ]
--- core/core/settings.py
+++ core/core/settings.py
@@ -140,3 +140,14 @@ AUTHENTICATION_BACKENDS = ['accounts.backends.EmailUsernameBackend']
 LOGIN_REDIRECT_URL = '/tasks/'
 LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/tasks/'
+
+# Email Config
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+#zfhu lfma xqzd yujw
+#vull eymo tocw odeg
