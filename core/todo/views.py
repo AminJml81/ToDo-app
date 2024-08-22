69d49626a8d379cee5c2eefbe6c25993b19236a7
Amin <Aminbest1381@gmail.com>
2024-08-22 17:16:38 +0330

    cleaned templates

--- core/todo/views.py
+++ core/todo/views.py
@@ -10,7 +10,7 @@ from todo.forms import TaskForm
 
 class ListTasksView(LoginRequiredMixin, ListView):
     model = Task
-    template_name = 'task-list.html'
+    template_name = 'todo/task-list.html'
     context_object_name = 'tasks'
 
     def get_queryset(self):
@@ -20,7 +20,7 @@ class ListTasksView(LoginRequiredMixin, ListView):
 
 class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
-    template_name = 'task-detail.html'
+    template_name = 'todo/task-detail.html'
     context_object_name = 'task'
 
     def get_queryset(self):
@@ -31,7 +31,7 @@ class TaskDetailView(LoginRequiredMixin, DetailView):
 class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
     form_class = TaskForm
-    template_name = 'task-update.html'
+    template_name = 'todo/task-update.html'
     context_object_name = 'task'
 
     
@@ -43,7 +43,7 @@ class TaskUpdateView(LoginRequiredMixin, UpdateView):
 class CreateTaskView(LoginRequiredMixin, CreateView):
     model = Task
     form_class = TaskForm 
-    template_name = 'task-create.html'
+    template_name = 'todo/task-create.html'
     success_url = reverse_lazy('todo:task-list')
         
     def post(self, requets, *args, **kwargs):
