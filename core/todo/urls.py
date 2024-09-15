c6d1872ec01749220a84f50d0540c972b7f55473
Amin <Aminbest1381@gmail.com>
2024-09-16 00:26:31 +0330

    fix api-v2 urls

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('<slug:slug>/', TaskDetailView.as_view(), name='task-detail'),
     path('<slug:slug>/delete/', DeleteTaskView.as_view(), name='task-delete'),
     path('<slug:slug>/update/', TaskUpdateView.as_view(), name='task-update'),
-    path('api/v1/', include('todo.api.v1.urls'))
+    path('api/v1/', include('todo.api.v1.urls')),
+    path('api/v2/', include('todo.api.v2.urls')),
 
 ]
