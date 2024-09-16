9eadc1a37bc832cadf0bcb00c446edde8c658b08
Amin <Aminbest1381@gmail.com>
2024-09-16 15:23:02 +0330

    clean v5 api views file

--- core/todo/api/v5/views.py
+++ core/todo/api/v5/views.py
@@ -5,30 +5,6 @@ from ...models import Task
 from ..serializers import TaskReadSerializer, TaskCreateUpdateSerializer
 
 
-# class ListCreateTaskGenericView(ModelViewSet):
-#     permission_classes = [IsAuthenticated]
-
-#     def get_serializer_class(self):
-#         if self.request.method == "POST":
-#             return TaskCreateUpdateSerializer
-#         return TaskReadSerializer
-    
-#     def get_queryset(self):
-#         return Task.objects.filter(user=self.request.user)
-        
-
-# class RetriveUpdateDeleteTaskGenericView(RetrieveUpdateDestroyAPIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def get_serializer_class(self):
-#         if self.request.method in ("PUT", "PATCH"):
-#             return TaskCreateUpdateSerializer
-#         return TaskReadSerializer
-        
-#     def get_queryset(self):
-#         return Task.objects.filter(user=self.request.user)
-
-
 class TaskViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     
